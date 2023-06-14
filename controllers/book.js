1e6969147c7bd5449b87d01a514c2a97473cfe71
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-06-14 16:39:23 +0200

    feat(book): getAllBooks & getOneBook func

---
+++ controllers/book.js
@@ -0,0 +1,17 @@
+const { error } = require('console');
+const Book = require('../models/book');
+
+const fs = require('fs');
+const book = require('../models/book');
+
+exports.getAllBooks = (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+};
