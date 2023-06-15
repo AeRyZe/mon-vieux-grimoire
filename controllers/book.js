08947f7b70adfdd0d5a4c0e025408c4da13b0528
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-06-15 15:20:01 +0200

    feat(book): deleteBook func

--- controllers/book.js
+++ controllers/book.js
@@ -60,3 +60,20 @@ exports.modifyBook = (req, res, next) => {
     })
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.deleteBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => {
+        if (book.userId == req.auth.userId) {
+            const filename = book.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Book.deleteOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
+                .catch(error => res.status(401).json({ error }));
+            })
+        } else {
+            return res.status(403).json({ message: 'Requête non-autorisée !' });
+        };
+    })
+    .catch(error => res.status(404).json({ error }));
+};
