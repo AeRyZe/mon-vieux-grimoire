70eb7849393168a0cdce8d709f64dc05d9c64980
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-06-15 15:18:20 +0200

    feat(book): getBestRating func

--- controllers/book.js
+++ controllers/book.js
@@ -1,13 +1,19 @@
-const { error } = require('console');
 const Book = require('../models/book');
 
 const fs = require('fs');
-const book = require('../models/book');
 
 exports.getAllBooks = (req, res, next) => {
     Book.find()
     .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({ error }));
+};
+
+exports.getBestRating = (req, res, next) => {
+    Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneBook = (req, res, next) => {
