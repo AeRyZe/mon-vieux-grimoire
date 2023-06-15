4e4c50736b36e23d78d7fd4521170969335c1e20
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-06-15 15:19:25 +0200

    feat(book): modifyBook func

--- controllers/book.js
+++ controllers/book.js
@@ -36,3 +36,27 @@ exports.createBook = (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    delete bookObject._id;
+
+    Book.findOne({ _id: req.params.id })
+    .then(book => {
+        if (book.userId == req.auth.userId) {
+            if (req.file) {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlinkSync(`images/${filename}`);
+            };
+            Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+            .catch(error => res.status(401).json({ error }));
+        } else {
+            return res.status(403).json({ message: 'Requête non-autorisée !' });
+        };
+    })
+    .catch(error => res.status(400).json({ error }));
+};
