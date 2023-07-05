c4803d9dfd75dbe816b626c79ad2d7c12e9a78a5
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 18:34:38 +0200

    fix(multer): adapted to new sharp middleware

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 
+
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
@@ -12,10 +13,11 @@ const storage = multer.diskStorage({
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split('.')[0].split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension)
-    }
+            const newFilename = file.originalname.split('.')[0].split(' ').join('_');
+            const extension = MIME_TYPES[file.mimetype];
+
+            callback(null, newFilename + Date.now() + '.' + extension);
+    },
 });
 
 module.exports = multer({ storage }).single('image')
