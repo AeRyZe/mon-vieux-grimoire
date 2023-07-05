eac8f2c2f1576ce7ec4b4ab08cec76a2282dd79d
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 20:48:02 +0200

    fix(book): post & put routes with sharp middleware

--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,7 @@ const bookCtrl = require('../controllers/book');
 
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
+const sharp = require('../middlewares/sharp-config');
 
 const express = require('express');
 const router = express.Router();
@@ -9,8 +10,8 @@ const router = express.Router();
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multer, sharp, bookCtrl.createBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.addNewRating);
 
