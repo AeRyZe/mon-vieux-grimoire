0fe3f7b382a7a44dcb8af6c8d102ec3489f8cb65
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-20 04:16:28 +0200

    fix(vercel): changed file names for deployment

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express'); // framework pour créer notre app
+const mongoose = require('mongoose'); // librairie pour se connecter à MongoDB
+const path = require('path'); // Outil pour travailler avec les paths vers fichiers et dossiers
+
+const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+
+const app = express();
+
+require('dotenv').config(); // Appel des variables d'environnements depuis .env
+
+mongoose.connect(`mongodb+srv://${process.env.USR_MONGO}:${process.env.PWD_MONGO}@cluster0.ldyi2wp.mongodb.net/`, // connection à MongoDB via mongoose
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => { // paramètrage des requêtes à API
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Origine des requêtes possible
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Headers utilisables
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Verbes HTTP utilisables
+  next();
+});
+
+app.use('/api/auth', userRoutes); // Routage pour users
+
+app.use('/api/books', bookRoutes); // Routage pour livres
+
+app.use('/images', express.static(path.join(__dirname, 'images'))); // Routage des images
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const express = require('express'); // framework pour créer notre app
-const mongoose = require('mongoose'); // librairie pour se connecter à MongoDB
-const path = require('path'); // Outil pour travailler avec les paths vers fichiers et dossiers
+const http = require('http');
+const app = require('./app');
 
-const userRoutes = require('./routes/user');
-const bookRoutes = require('./routes/book');
+const normalizePort = val => { // vérifie que la reqûete de port soit correcte
+  const port = parseInt(val, 10);
 
-const app = express();
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
 
-require('dotenv').config(); // Appel des variables d'environnements depuis .env
+const errorHandler = error => { // gère les éventuelles erreurs
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-mongoose.connect(`mongodb+srv://${process.env.USR_MONGO}:${process.env.PWD_MONGO}@cluster0.ldyi2wp.mongodb.net/`, // connection à MongoDB via mongoose
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const server = http.createServer(app);
 
-app.use(express.json());
-
-app.use((req, res, next) => { // paramètrage des requêtes à API
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Origine des requêtes possible
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Headers utilisables
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Verbes HTTP utilisables
-  next();
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-app.use('/api/auth', userRoutes); // Routage pour users
-
-app.use('/api/books', bookRoutes); // Routage pour livres
-
-app.use('/images', express.static(path.join(__dirname, 'images'))); // Routage des images
-
-module.exports = app;
+server.listen(port);
